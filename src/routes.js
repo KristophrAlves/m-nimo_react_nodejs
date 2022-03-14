9adbfc22f730f58aaccae13ed45b41cf4ee078b9
kristophr Alves <kristophralves@gmail.com>
2022-03-14 14:34:31 -0300

    teste .env

---
+++ src/routes.js
@@ -0,0 +1,9 @@
+import { Router } from "express";
+
+import helloController from "./controllers/helloController";
+
+const routes = new Router();
+
+routes.get ('/hello', helloController.index);
+
+export default routes;
